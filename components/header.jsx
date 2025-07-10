44021187703ae41719d34bad76d0f0092834bd19
Ankit Kharel <ankeetkharel@gmail.com>
2025-07-10 11:38:50 +0545

    clerk authentication added

---
+++ components/header.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full border-b backdrop-blur-md z-10 supports-[backdrop-filter]: bg-background/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo-single.png"
+            alt="Medimeet logo"
+            width={50}
+            height={50}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div>
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton></SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
