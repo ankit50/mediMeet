b48daa347b9deb40db2bb549b7d61f628c3029b1
Ankit Kharel <ankeetkharel@gmail.com>
2025-07-15 13:07:07 +0545

    doctor verfication form created

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -17,6 +17,19 @@ import { setUserRole } from "@/actions/onboarding";
 import { Router } from "next/router";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { Yomogi } from "next/font/google";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { SPECIALTIES } from "@/lib/specialities";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
 const doctorFormSchema = z.object({
   speciality: z.string().min(1, "Speciality is required"),
   experience: z.number().min(1, "Experience must be at least one year"),
@@ -41,6 +54,8 @@ const OnboardingPage = () => {
   } = useForm({
     resolver: zodResolver(doctorFormSchema),
   });
+
+  const specialityValue = watch("speciality");
 
   const handlePatientSelection = async () => {
     if (loading) return;
@@ -116,8 +131,124 @@ const OnboardingPage = () => {
       </div>
     );
   }
-  if (step == "doctor-form") {
-    return <>dsfdgsdrfg</>;
+  if (step === "doctor-form") {
+    return (
+      <Card className="border-emerald-900/20">
+        <CardContent className="pt-6 ">
+          <div className=" mb-6">
+            <CardTitle className="text-2xl font-bold text-white mb-2">
+              Complete Your Doctor Profile
+            </CardTitle>
+            <CardDescription>
+              Please provide your professional details for verification
+            </CardDescription>
+          </div>
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label htmlFor="speciality">Medical Speciality</Label>
+              <Select
+                value={specialityValue}
+                onValueChange={(value) => setValue("speciality", value)}
+              >
+                <SelectTrigger id="speciality">
+                  <SelectValue placeholder="Select Your speciality" />
+                </SelectTrigger>
+                <SelectContent>
+                  {SPECIALTIES.map((spec) => {
+                    return (
+                      <SelectItem key={spec.name} value={spec.name}>
+                        <div className="flex items-center gap-2">
+                          <span className="text-emerald-400">{spec.icon}</span>
+                          {spec.name}
+                        </div>
+                      </SelectItem>
+                    );
+                  })}
+                </SelectContent>
+              </Select>
+              {errors.speciality && (
+                <p className="text-sm font-medium text-red-500 mt-1">
+                  {errors.speciality.message}
+                </p>
+              )}
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="experience">Years of Experience</Label>
+              <Input
+                id="experience"
+                type="number"
+                placeholder="eg. 5"
+                {...register("experience", { valueAsNumber: true })}
+              />
+              {errors.experience && (
+                <p className="text-sm font-medium text-red-500 mt-1">
+                  {errors.experience.message}
+                </p>
+              )}
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="credentialUrl">Link to Credential Document</Label>
+              <Input
+                id="credentialUrl"
+                type="url"
+                placeholder="https://example.com/my-medical-degree.pdf"
+                {...register("credentialUrl")}
+              />
+              {errors.credentialUrl && (
+                <p className="text-sm font-medium text-red-500 mt-1">
+                  {errors.credentialUrl.message}
+                </p>
+              )}
+              <p className="text-sm text-muted-foreground">
+                Please provide a link to your medical degree or certification
+              </p>
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="description">Description of Your Services</Label>
+              <Textarea
+                id="description"
+                placeholder="Describe your expertise, services, and approach to patient care..."
+                rows="4"
+                {...register("description")}
+              />
+              {errors.description && (
+                <p className="text-sm font-medium text-red-500 mt-1">
+                  {errors.description.message}
+                </p>
+              )}
+            </div>
+
+            <div className="pt-2 flex items-center justify-between">
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => setStep("choose-role")}
+                className="border-emerald-900/30"
+                disabled={loading}
+              >
+                Back
+              </Button>
+              <Button
+                type="submit"
+                className="bg-emerald-600 hover:bg-emerald-700"
+              >
+                {loading ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    Processing....
+                  </>
+                ) : (
+                  "Conitnue as Patient"
+                )}
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    );
   }
 };
 
